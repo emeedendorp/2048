2b9abed3e29c01d90046fa9c129b24578a9432d8
Eppo Meedendorp <emeedendorp@gmail.com>
2015-06-18 16:57:20 +0200

    changed something

--- 2048/src/_2048/Colorscheme.java
+++ 2048/src/_2048/Colorscheme.java
@@ -7,7 +7,7 @@ public class Colorscheme {
 	HashMap<Integer, Color> hash = new HashMap<>();
 
 	public Colorscheme(){
-	// Put three keys with values.
+	hash.put(0, Color.yellow);	
 	hash.put(2, Color.white);
 	hash.put(4, Color.gray);
 	hash.put(8, Color.darkGray);
@@ -19,15 +19,29 @@ public class Colorscheme {
 	}
 	
 	Color getColor(int value){
-		Color color = Color.GREEN;
-		try{
-		color = hash.get(value);
+		Color color;
+		if (hash.get(value) !=null){
+			color = hash.get(value);
 		}
-		catch(NullPointerException e){
-			
+		else{
+			color = Color.GREEN;
 		}
-		finally{}
 		return color ;
 		
 	}
+	void setColor(int value, Color color){
+		hash.put(value, color);
+	}
+	
+	public Colorscheme(int hue){
+	hash.put(0, Color.yellow);	
+	hash.put(2, Color.white);
+	hash.put(4, Color.gray);
+	hash.put(8, Color.darkGray);
+	hash.put(16, Color.black);
+	hash.put(32, Color.pink);
+	hash.put(64, Color.orange);
+	hash.put(128, Color.RED);
+	hash.put(256, Color.blue);
+	}	
 }
