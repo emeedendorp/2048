1396613a81d47f2a40901dc26c82914d865e2377
RJM <RJM@192.168.178.10>
2016-01-26 22:43:14 +0100

    tried out updating JButton without succes

--- 2048/src/_2048/Infoboard.java
+++ 2048/src/_2048/Infoboard.java
@@ -7,16 +7,23 @@ import javax.swing.JPanel;
 
 public class Infoboard extends JPanel{
 	private static final long serialVersionUID = 1L;
-	JButton start = new JButton("New Game");
+	String buttontext;
+	JButton start = new JButton(buttontext);
 	Control control;
 	
 	public Infoboard(Control control){
 		start.addActionListener(new StartButtonHandler(control));
+		buttontext = "Game starten";
+		start.setText(buttontext);
 		add(start);
 		this.control = control;
 	}
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
+		if(control.started){
+		start.setText(buttontext);
+		}
 		g.drawString("Score: " + control.getScore(), 10, 50);
+		
 	}
 }
